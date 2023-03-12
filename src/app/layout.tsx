42aac7aaa5c49158f204d812ca91f6ccd2f63421
Xocash695 <akash.kallumkal@gmail.com>
2023-03-12 19:34:51 -0400

    Adding text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
+"use client"
 import './globals.css'
-
+import Navbar from "@/components/Navbar";
 export default function RootLayout({
   children,
 }: {
@@ -12,7 +13,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Navbar/>
+        {children}
+      </body>
     </html>
   )
 }
